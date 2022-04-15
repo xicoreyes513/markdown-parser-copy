a4316851b247c69e9c651aa3a8ae55d00c5817fe
xicoreyes513 <100896310+xicoreyes513@users.noreply.github.com>
2022-04-15 13:05:08 -0700

    Fixes "()" within a link problem

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,9 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (markdown.substring(openParen, closeParen).contains("(")){
+                closeParen = markdown.indexOf(")", closeParen + 1);
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
