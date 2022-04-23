f22293427ef0d1208d8ddd355719749dd4f836cb
xicoreyes513 <100896310+xicoreyes513@users.noreply.github.com>
2022-04-23 14:21:17 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -24,6 +24,9 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (openParen == closeBracket + 1){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
             
         }
