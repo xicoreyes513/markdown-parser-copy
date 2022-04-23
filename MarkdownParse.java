a3ee8e0c44343152610a430327b7538713fcab98
xicoreyes513 <100896310+xicoreyes513@users.noreply.github.com>
2022-04-23 13:56:35 -0700

    Text at the end of file bug

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -14,7 +14,12 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-
+            if (markdown.indexOf("[", currentIndex) == -1 || 
+                markdown.indexOf("]", currentIndex) == -1 ||
+                markdown.indexOf("(", currentIndex) == -1 ||
+                markdown.indexOf(")", currentIndex) == -1){
+                    break;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
